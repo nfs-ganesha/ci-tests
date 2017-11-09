37778a54a80c907d74f672f436c7456307d6ecc9
mpadariy <32087537+mpadariy@users.noreply.github.com>
2017-11-09 14:03:47 +0530

    Changed umount strategy
    Updated umount startegy from lazy umount to normal. Since lazy umount may bypass any server issues that results during umount.

--- iozone/client.sh
+++ iozone/client.sh
@@ -39,7 +39,7 @@ else
         exit $ret;
 fi
 
-umount -l /mnt/ganesha
+umount /mnt/ganesha
 
 mount -t nfs -o vers=4.0 ${SERVER}:${EXPORT} /mnt/ganesha
 
@@ -63,7 +63,7 @@ else
         exit $ret;
 fi
 
-umount -l /mnt/ganesha
+umount /mnt/ganesha
 
 mount -t nfs -o vers=4.1 ${SERVER}:${EXPORT} /mnt/ganesha
 
@@ -87,5 +87,5 @@ else
         exit $ret;
 fi
 
-umount -l /mnt/ganesha
+umount /mnt/ganesha
 
