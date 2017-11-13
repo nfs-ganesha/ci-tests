b54ca4fff71ea88d217c704bab301a9e975cc83b
Mohammed Azhar Padariyakam <mpadariy@redhat.com>
2017-11-13 15:39:41 +0530

    Added Command For Entry to /etc/fstab

--- ciTest1/client_script.sh
+++ ciTest1/client_script.sh
@@ -1,8 +1,38 @@
 #!/bin/sh
+#
+# Environment variables used:
+#  - SERVER: hostname or IP-address of the NFS-server
+#  - EXPORT: NFS-export to test (should start with "/")
 
-echo "Hello From Client"
+# enable some more output
+set -x
 
-echo "Contents of FSTAB"
-echo "-----------------"
+[ -n "${SERVER}" ]
+[ -n "${EXPORT}" ]
 
+# install build and runtime dependencies
+yum -y install nfs-utils time centos-release-gluster
+
+mkdir -p /mnt/ganesha
+
+yum --enablerepo=centos-gluster*test -y install iozone
+
+mount -t nfs -o vers=3 ${SERVER}:${EXPORT} /mnt/ganesha
+
+cd /mnt/ganesha
+
+echo "Hello World" > testFile.txt
+
+cd / && umount /mnt/ganesha
+
+fstabEntry=`echo -e $SERVER:/$EXPORT "\t" /mnt/ganesha "\t" nfs "\t" defaults "\t" 1 "\t" 1`
+
+echo "FSTAB ENTRY VARIABLE"
+echo "$fstabEntry"
+
+echo "$fstabEntry" >> /etc/fstab
+
+echo "FSTAB FILE"
 cat /etc/fstab
+
+
