8f84826912e5d0ab8bafd8a1f38d715932b97576
Girjesh Rajoria <grajoria@redhat.com>
2017-11-27 18:03:09 +0530

    Cache invalidation Test for nfs-ganesha
    Cache invalidation test for nfs-ganesha on mount v3, v4.0 and v4.1
    
    Signed-off-by: Girjesh Rajoria <grajoria@redhat.com>

---
+++ cache-invalidation/client.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+#
+# Environment variables used:
+#  - SERVER: hostname or IP-address of the NFS-server
+#  - EXPORT: NFS-export to test (should start with "/")
+
+# if any command fails, the script should exit
+set -e
+
+# enable some more output
+set -x
+
+if [ $1 -eq 1 ]
+then
+    [ -n "${SERVER}" ]
+    [ -n "${EXPORT}" ]
+    
+    # install build and runtime dependencies
+    echo "Install build and runtime dependencies"
+    yum -y install nfs-utils time
+
+    echo "--------------------------------------------------"
+    echo "Running test on Mount Version $2"
+    echo "--------------------------------------------------"
+
+    # mount
+    mkdir -p /mnt/nfs
+    mount -t nfs -o vers=$2 ${SERVER}:${EXPORT} /mnt/nfs
+    status=$?
+    if [ $status -ne 0 ]
+    then
+        echo "Failed Mounting for version 3 on client-2"
+        echo "Cache Invalidation Test: FAILURE"
+        exit $status
+    fi
+
+elif [ $1 -eq 2 ]
+then
+    touch /mnt/nfs/file1.txt
+    > /mnt/nfs/file1.txt
+    echo "We Are" >> /mnt/nfs/file1.txt
+
+elif [ $1 -eq 3 ]
+then
+    content=$(cat /mnt/nfs/file1.txt)
+    if [ "$content" == "We Are" ]
+    then
+        echo "REDHAT" >> /mnt/nfs/file1.txt
+    else
+        echo "Cache Invalidation Test: FAILURE"
+        exit -1
+    fi
+    #unmount on client-2
+    umount /mnt/nfs
+
+elif [ $1 -eq 4 ]
+then
+    content=$(cat /mnt/nfs/file1.txt)
+    if [ "$content" != "We Are
+REDHAT" ]
+    then
+        echo "Cache Invalidation Test: FAILURE"
+        exit -1
+    fi
+    #unmount on client-1
+    umount /mnt/nfs
+fi
