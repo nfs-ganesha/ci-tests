03d70be3b35fef16965f90568a4e7109cdc112dd
Girjesh Rajoria <32087575+grajoria@users.noreply.github.com>
2017-11-30 22:31:33 +0530

    Update client.sh

--- code-compilation/client.sh
+++ code-compilation/client.sh
@@ -17,71 +17,45 @@ set -x
 echo "Install build and runtime dependencies"
 yum -y install nfs-utils git gcc time
 
-echo "--------------------------------------------------"
-echo "Running test on Mount Version 3"
-echo "--------------------------------------------------"
+# flag for commands which should run only once
+once=0
 
-# mount
-mkdir -p /mnt/nfs
-mount -t nfs -o vers=3 ${SERVER}:${EXPORT} /mnt/nfs
-cd /mnt/nfs
-yum -y install bison flex cmake gcc-c++ libacl-devel krb5-devel dbus-devel libnfsidmap-devel libwbclient-devel libcap-devel libblkid-devel rpm-build redhat-rpm-config
-git clone https://review.gerrithub.io/ffilz/nfs-ganesha
-cd nfs-ganesha
-git checkout next
-git submodule update --init || git submodule sync
-cd ..
-mkdir ganeshaBuild
-cd ganeshaBuild
-cmake -DDEBUG_SYMS=ON -DUSE_FSAL_GLUSTER=ON -DCURSES_LIBRARY=/usr/lib64 -DCURSES_INCLUDE_PATH=/usr/include/ncurses -DCMAKE_BUILD_TYPE=Maintainer -DUSE_DBUS=ON /mnt/nfs/nfs-ganesha/src
-make -j4
-make install
-cd ..
-rm -rf ganeshaBuild
+for ver in 3 4.0 4.1
+do
+    echo "--------------------------------------------------"
+    echo "Running test on Mount Version $ver"
+    echo "--------------------------------------------------"
 
-#unmount
-umount -l /mnt/nfs
+    mkdir -p /mnt/nfs
+    # mount
+    mount -t nfs -o vers=$ver ${SERVER}:${EXPORT} /mnt/nfs
+    cd /mnt/nfs
+    if [ $once -eq 0 ]
+    then
+        yum -y install bison flex cmake gcc-c++ libacl-devel krb5-devel dbus-devel libnfsidmap-devel libwbclient-devel libcap-devel libblkid-devel rpm-build redhat-rpm-config
+        git clone https://review.gerrithub.io/ffilz/nfs-ganesha
+    fi
+    cd nfs-ganesha
+    git checkout next
+    if [ $once -eq 0 ]
+    then
+        git submodule update --init || git submodule sync
+        once=1
+    fi
+    cd ..
+    mkdir ganeshaBuild
+    cd ganeshaBuild
+    cmake -DDEBUG_SYMS=ON -DUSE_FSAL_GLUSTER=ON -DCURSES_LIBRARY=/usr/lib64 -DCURSES_INCLUDE_PATH=/usr/include/ncurses -DCMAKE_BUILD_TYPE=Maintainer -DUSE_DBUS=ON /mnt/nfs/nfs-ganesha/src
+    status=$?
+    if [ $status -ne 0 ]
+    then
+        echo "FAILURE: cmake failed"
+    fi
+    make -j4
+    make install
+    cd ..
+    rm -rf ganeshaBuild
 
-
-echo "--------------------------------------------------"
-echo "Running test on Mount Version 4.0"
-echo "--------------------------------------------------"
-
-# mount
-mkdir -p /mnt/nfs
-mount -t nfs -o vers=4.0 ${SERVER}:${EXPORT} /mnt/nfs
-cd /mnt/nfs
-cd nfs-ganesha
-git checkout next
-cd ..
-mkdir ganeshaBuild
-cd ganeshaBuild
-cmake -DDEBUG_SYMS=ON -DUSE_FSAL_GLUSTER=ON -DCURSES_LIBRARY=/usr/lib64 -DCURSES_INCLUDE_PATH=/usr/include/ncurses -DCMAKE_BUILD_TYPE=Maintainer -DUSE_DBUS=ON /mnt/nfs/nfs-ganesha/src
-make -j4
-make install
-cd ..
-rm -rf ganeshaBuild
-
-#unmount
-umount -l /mnt/nfs
-
-
-echo "--------------------------------------------------"
-echo "Running test on Mount Version 4.1"
-echo "--------------------------------------------------"
-
-# mount
-mkdir -p /mnt/nfs
-mount -t nfs -o vers=4.1 ${SERVER}:${EXPORT} /mnt/nfs
-cd /mnt/nfs
-cd nfs-ganesha
-git checkout next
-cd ..
-mkdir ganeshaBuild
-cd ganeshaBuild
-cmake -DDEBUG_SYMS=ON -DUSE_FSAL_GLUSTER=ON -DCURSES_LIBRARY=/usr/lib64 -DCURSES_INCLUDE_PATH=/usr/include/ncurses -DCMAKE_BUILD_TYPE=Maintainer -DUSE_DBUS=ON /mnt/nfs/nfs-ganesha/src
-make -j4
-make install
-
-#unmount
-umount -l /mnt/nfs
+    #unmount
+    umount -l /mnt/nfs
+done
