78c93c481191ac6dcd042f634d235f5144bb7f39
Jack Halford <jack@crans.org>
2018-11-21 17:04:15 +0100

    first review published, nfstest in place

--- client/00-common.sh
+++ client/00-common.sh
@@ -3,5 +3,5 @@ set -xe
 export SERVER=${SSH_CLIENT%% *}
 apt install nfs-common
 mkdir -p /nfs-test
-umount /nfs-test || true
-mount -t nfs ${SERVER}:/nfs-test /nfs-test
+mount -o remount || mount -t nfs ${SERVER}:/nfs-test /nfs-test
+umount /nfs-test
