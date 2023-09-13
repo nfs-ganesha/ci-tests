2838a3190ba5a920a5628e3b12398bfb1a99dfa5
Rakshitha Kamath <rkamath@redhat.com>
2023-09-13 17:35:44 +0530

    Update trigger-cthon04-on-new-patch
    Signed-off-by: Rakshitha Kamath <rkamath@redhat.com>

--- build_scripts/small-file/client.sh
+++ build_scripts/small-file/client.sh
@@ -33,13 +33,13 @@ do
 
 	for j in $Operation_List
 	do
-          timeout -s SIGKILL 240s ./smallfile_cli.py --files 200 --threads 20 --file-size 64 --hash-into-dirs Y --top $mount_pt --operation $j 
-          TIMED_OUT=$?
-          if [ $TIMED_OUT == 137 ]; then
-            echo -e "The process timed out after 4 minute!\nLooks like the Server process to see if it has crashed!"
-            exit 1
-          fi
-          #./smallfile_cli.py --files 200 --threads 20 --file-size 64 --hash-into-dirs Y --top $mount_pt --operation $j
+          #timeout -s SIGKILL 240s ./smallfile_cli.py --files 200 --threads 20 --file-size 64 --hash-into-dirs Y --top $mount_pt --operation $j 
+          #TIMED_OUT=$?
+          #if [ $TIMED_OUT == 137 ]; then
+          #  echo -e "The process timed out after 4 minute!\nLooks like the Server process to see if it has crashed!"
+          #  exit 1
+          #fi
+          ./smallfile_cli.py --files 200 --threads 20 --file-size 64 --hash-into-dirs Y --top $mount_pt --operation $j
 	done
 done
 
