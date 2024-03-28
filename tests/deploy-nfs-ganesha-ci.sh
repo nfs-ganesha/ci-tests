c770b9498d6bb467201c748cc97457ee9500ec57
Rakshitha Kamath <rakshithakamath@Rakshithas-MacBook-Pro.local>
2024-03-28 11:33:52 +0530

    Update jobs

--- tests/deploy-nfs-ganesha-ci.sh
+++ tests/deploy-nfs-ganesha-ci.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sed "s/JENKINS_API_KEY/$JENKINS_API_KEY/g" tests/jenkins.conf > jobs/jenkins.ini
-jenkins-jobs --conf jobs/jenkins.ini update jobs
+jenkins-jobs --allow-empty-variables --conf jobs/jenkins.ini update jobs/:globals/macros/
