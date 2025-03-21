bbc5bdbf8ff80c2564e2cd8465e9955670473e11
Pragadeeswaran Sathyanarayanan <pragadeeswaran.sathyanarayanan@ibm.com>
2025-03-21 20:07:58 +0530

    Fix clang-format failure to execute
    Earlier checkpatch was executed on the runtime / jenkins-agent,
    now we are requesting for a node to enable us to execute the
    checkpatch tasks.
    
    This shift is primarily due our inability to install any packages
    on the runtime / jenkins-agent. We require `clang-format` to be
    installed for linting.
    
    Signed-off-by: Pragadeeswaran Sathyanarayanan <pragadeeswaran.sathyanarayanan@ibm.com>

---
+++ build_scripts/common/check-patch.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# This script has the following workflow
+#   1. Checkout the patch reference
+#   2. Run clang format against the review.
+#   3. Execute checkpatch
+
+# It requires the following environment variables to be set
+#   GERRIT_REFSPEC
+#   GERRIT_PATCHSET_REVISION
+#   GERRIT_PUBLISH
+#   GERRITHUB_KEY
+#   GERRIT_USER
+#   JOB_URL
+
+set -o pipefail 
+set -x
+
+# Avoid failures
+GERRIT_PUBLISH=${GERRIT_PUBLISH:-'false'}
+GERRIT_HOST=${GERRIT_HOST:-"review.gerrithub.io"}
+GERRIT_PORT=${GERRIT_PORT:-"29418"}
+GERRIT_PROJECT=${GERRIT_PROJECT:-"ffilz/nfs-ganesha"}
+
+# Checkout the patch
+if [ ! -d nfs-ganesha ]; then
+    git_project=$(basename "${GERRIT_PROJECT}")
+    git_url="https://${GERRIT_HOST}/${GERRIT_PROJECT}"
+    git init ${git_project}
+    pushd ${git_project}
+    git fetch --depth=1 "${git_url}" "${GERRIT_REFSPEC}"
+    popd
+fi
+
+# Install git-clang-format
+yum -y install git-clang-format
+
+pushd nfs-ganesha
+git checkout -b "${GERRIT_REFSPEC}" FETCH_HEAD
+git clang-format -v \
+    --diff \
+    --style file:src/.clang-format \
+    --extensions c,cc,cpp,h,hpp \
+    HEAD~1
+RETURN_VALUE=$?
+popd
+
+# Post message
+case ${RETURN_VALUE} in
+    0)
+        MESSAGE="${JOB_URL}: Success."
+        VERIFIED="--verified +1"
+        NOTIFY="--notify NONE"
+        EXIT=0
+        ;;
+    1)
+        MESSAGE="${JOB_URL}: Failed"
+        VERIFIED=''
+        NOTIFY="--notify all"
+        EXIT=1
+        ;;
+    *)
+        MESSAGE="${job_url}: UNKNOWN"
+        VERIFIED=''
+        NOTIFY="--notify NONE"
+        EXIT=1
+        ;;
+esac
+
+if [ "${GERRIT_PUBLISH}" == "true" ]; then
+    ssh \
+        -l ${GERRIT_USER} \
+        -i ${GERRITHUB_KEY} \
+        -o StrictHostKeyChecking=no \
+        -p ${GERRIT_PORT} \
+        ${GERRIT_HOST} \
+        gerrit review \
+            --message "'${MESSAGE}'" \
+            --project ${GERRIT_PROJECT} \
+            ${VERIFIED} \
+            ${NOTIFY} \
+            ${GERRIT_PATCHSET_REVISION}
+else
+    echo "Clang format review is not posted"
+fi
+
+publish_checkpatch() {
+    local SSH_GERRIT="ssh -p 29418 -i $GERRITHUB_KEY $GERRIT_USER@review.gerrithub.io"
+
+    if [[ "$GERRIT_PUBLISH" == "true" ]]; then
+        tee /proc/$$/fd/1 | \
+        $SSH_GERRIT "gerrit review --json --project ffilz/nfs-ganesha $REVISION"
+    else
+        echo "Would have submit:"
+        echo -n "echo '"
+        cat
+        echo "' | $SSH_GERRIT \"gerrit review --json --project ffilz/nfs-ganesha $REVISION\""
+  fi 
+}
+
+pushd nfs-ganesha/src/scripts
+# cd to ~/checkpatch for checkpatch.pl as a hack to get config without modifying $HOME
+GIT_DIR=~/nfs-ganesha/.git git show --format=email  | \
+    ./checkpatch.pl -q - | \
+    python ~/checkpatch-to-gerrit-json.py | \
+    publish_checkpatch
+popd
--- jobs/scripts/checkpatch.sh
+++ jobs/scripts/checkpatch.sh
@@ -1,96 +1,33 @@
 #!/bin/bash
 
-# This script has the following workflow
-#   1. Checkout the patch reference
-#   2. Run clang format against the review.
-#   3. Execute checkpatch
+# This script executes the linter scripts against the given patchset
 set -o pipefail 
 set -x
 
-if [[ -n "$GERRIT_REFSPEC" ]]; then
-    GERRIT_REF="$GERRIT_REFSPEC"
-    REVISION="$GERRIT_PATCHSET_REVISION"
-    GERRIT_PUBLISH=true
-fi
+JOB_URL="${JENKINS_URL}/job/checkpatch/${BUILD_NUMBER}/console"
+SSH_OPTIONS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+SERVER_IP=$(cat ${WORKSPACE}/hosts | sed -n '1p')
 
-if [ ! -d nfs-ganesha ]; then
-    GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i $GERRITHUB_KEY" git \
-        clone --depth=1 \
-        -o gerrit \
-        ssh://$GERRIT_USER@review.gerrithub.io:29418/ffilz/nfs-ganesha.git -v
-fi
+# Copy checkpatch-to-gerrit-json file and check-patch
+scp ${SSH_OPTIONS} \
+    ${WORKSPACE}/ci-tests/build_scripts/common/checkpatch-to-gerrit-json.py \
+    root@${SERVER_IP}:checkpatch-to-gerrit-json.py
+scp ${SSH_OPTIONS} ${WORKSPACE}/ci-tests/build_scripts/common/check-patch.sh \
+    root@${SERVER_IP}:check-patch.sh
+scp ${SSH_OPTIONS} ${GERRITHUB_KEY} root@${SERVER_IP}:gerrit-key
+ssh -t ${SSH_OPTIONS} -l root ${SERVER_IP} 'chmod 0600 ~/gerrit-key'
 
-( cd nfs-ganesha && git fetch gerrit $GERRIT_REF && git checkout $REVISION )
-
-job_url="${JENKINS_URL}/job/checkpatch/${BUILD_NUMBER}/console"
-
-# Install git-clang-format
-yum -y install git-clang-format
-
-pushd nfs-ganesha
-git clang-format -v \
-    --diff \
-    --style file:src/.clang-format \
-    --extensions c,cc,cpp,h,hpp \
-    HEAD~1
-RETURN_VALUE=$?
-popd
-
-# Post message
-case ${RETURN_VALUE} in
-    0)
-        MESSAGE="${job_url}: Success."
-        VERIFIED="--verified +1"
-        NOTIFY="--notify NONE"
-        EXIT=0
-        ;;
-    1)
-        MESSAGE="${job_url}: Failed"
-        VERIFIED='--verified -1'
-        NOTIFY="--notify all"
-        EXIT=1
-        ;;
-    *)
-        MESSAGE="${job_url}: UNKNOWN"
-        VERIFIED=''
-        NOTIFY="--notify NONE"
-        EXIT=1
-        ;;
-esac
-
-if [ "${GERRIT_PUBLISH}" == "true" ]; then
-    ssh \
-        -l ${GERRIT_USER} \
-        -i ${GERRITHUB_KEY} \
-        -o StrictHostKeyChecking=no \
-        -p ${GERRIT_PORT} \
-        ${GERRIT_HOST} \
-        gerrit review \
-            --message "'${MESSAGE}'" \
-            --project ${GERRIT_PROJECT} \
-            ${VERIFIED} \
-            ${NOTIFY} \
-            ${GERRIT_PATCHSET_REVISION}
-else
-    echo "Clang format review is not posted"
-fi
-
-publish_checkpatch() {
-    local SSH_GERRIT="ssh -p 29418 -i $GERRITHUB_KEY $GERRIT_USER@review.gerrithub.io"
-
-    if [[ "$GERRIT_PUBLISH" == "true" ]]; then
-        tee /proc/$$/fd/1 | \
-        $SSH_GERRIT "gerrit review --json --project ffilz/nfs-ganesha $REVISION"
-    else
-        echo "Would have submit:"
-        echo -n "echo '"
-        cat
-        echo "' | $SSH_GERRIT \"gerrit review --json --project ffilz/nfs-ganesha $REVISION\""
-  fi 
-}
-
-# cd to ~/checkpatch for checkpatch.pl as a hack to get config without modifying $HOME
-GIT_DIR=nfs-ganesha/.git git show --format=email  | \
-    (cd $WORKSPACE/ci-tests/build_scripts/checkpatch && ./checkpatch.pl -q - || true ) | \
-    python $WORKSPACE/ci-tests/build_scripts/checkpatch/checkpatch-to-gerrit-json.py    | \
-    publish_checkpatch
+# Execute check-patch on the reserved node
+# Indentation is not applied on purpose due to use of double quotes
+ssh -t ${SSH_OPTIONS} \
+    -l root \
+    ${SERVER_IP} \
+    "GERRIT_HOST='${GERRIT_HOST}' \
+GERRIT_PROJECT='${GERRIT_PROJECT}' \
+GERRIT_REFSPEC='${GERRIT_REFSPEC}' \
+GERRIT_PATCHSET_REVISION='${GERRIT_PATCHSET_REVISION}' \
+GERRIT_PUBLISH='${GERRIT_PUBLISH}' \
+GERRITHUB_KEY='/root/gerrit-key' \
+GERRIT_USER='${GERRIT_USER}' \
+JOB_URL='${JOB_URL}' \
+bash -x check-patch.sh"
